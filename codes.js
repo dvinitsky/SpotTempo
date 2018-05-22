99134d617ed88c2538f8fd61daf97e74667f1e4d
Daniel Vinitsky <daniel.vinitsky@gmail.com>
2018-05-22 15:44:56 -0500

    Playing around with authorization for Spotify

---
+++ codes.js
@@ -0,0 +1,70 @@
+const fs = require('fs');
+const https = require('https');
+const express = require('express'),
+  app = express(),
+  path = require('path');
+
+  
+const clientId = '200fe6a2e65643b4bada24a59cebc2cb';
+const clientSecret = 'c5082a4127ae4ae7b61dd87abe544784';
+const redirectUri = 'http://localhost:3000/';
+
+
+app.set('port', process.env.PORT || 5000);       //set port variable
+
+//allows front-end to request clientId and clientSecret
+app.use((req, res, next) => {
+  res.append('Access-Control-Allow-Origin', ['*']);
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.append('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+
+app.get('/clientId', (req, res) => {
+  console.log('Client id has been reached. About to send ' + clientId);
+  res.status(200).send(clientId);
+});
+
+app.get('/clientSecret', (req, res) => {
+  res.status(200).send(clientSecret);
+});
+
+app.get('/spotifylogin', (req, res) => {
+
+  console.log('About to contact Spotify');
+  let url = 'https://accounts.spotify.com/authorize?client_id=200fe6a2e65643b4bada24a59cebc2cb&response_type=code&redirect_uri=http://localhost:3000/&scope=playlist-read-private%20playlist-modify-private&state=1777263456';
+
+  let result = res.redirect(url);
+  res.status(200).send(result);
+  /*
+  const newRequest = https.get(url, (response) => {
+    let data = '';
+    response.on('data', (chunk) =>{
+      data += chunk;
+    });
+
+    response.on('end', () => {
+      console.log('DATA: ' + data);
+      console.log('Done reading.');
+      return data;
+      //res.status(200).send(response);
+    });
+
+  });
+
+  newRequest.on('error', (err) => {
+    console.log('oo');
+  });
+*/
+});
+
+
+app.listen(app.get('port'), function(){
+  console.log('Listening on port ' + app.get('port'));
+});
+
+////////////////////////////////////////////////
+
+
+//const loginPostRequestURL = 'https://accounts.spotify.com/api/token';
--- src/Components/SpotifyHandler.js
+++ src/Components/SpotifyHandler.js
@@ -1,12 +1,5 @@
 const playlistURL = 'https://api.spotify.com/v1/users/{user_id}/playlists/{playlist_id}/tracks';
-const clientId = '200fe6a2e65643b4bada24a59cebc2cb';
-
-//MOVE THE SECRET KEY TO A SECURE LOCATION, LIKE A BACKEND SERVICE (NODE.JS)
-const clientSecret = 'c5082a4127ae4ae7b61dd87abe544784';
-
-const redirectUri = 'https://www.google.com';
-
-let randomState = length => {
+function randomState (length) {
   var text = '';
   var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
@@ -15,71 +8,63 @@ let randomState = length => {
   }
   console.log(text);
   return text;
-};
+}
+//&state=" + randomState(10)
 
-const loginGetRequestURL = 'https://accounts.spotify.com/authorize/?client_id=' + clientId + "&response_type=code&redirect_uri=" + redirectUri; //+ "&scope=playlist-read-private%20playlist-modify-private&state=" + randomState;
+////////////////////////////////////
 
-const loginPostRequestURL = 'https://accounts.spotify.com/api/token';
+export function loginUser(){
+  async function getCodes(){
+    try{
+      let response = await fetch('http://localhost:8888/login');
+      if (response.ok) {
+        let textResponse = await response.text();
+        return textResponse;
+      } throw new Error('Request Failed.');
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  getCodes();
 
-let myHeaders = new Headers({
-  'Access-Control-Allow-Origin': true
-});
-
-let myObj = {
-  method: 'GET',
-  mode: 'cors',
-  headers: myHeaders
+    /*.then(jsonResponse => {
+      console.log('auth code: ' + jsonResponse);
+      fetch(loginPostRequestURL, {
+        method: 'POST',
+        body: JSON.stringify({id: '200'}),
+        grant_type: 'authorization_code',
+        code: jsonResponse.code,
+        redirect_uri: redirectUri,
+        client_id: clientId,
+        client_secret: clientSecret
+      }).then(response => {                  
+        if(response.ok) {
+          return response.json();                                                
+        }                                                                          
+        throw new Error('Request failed!');
+      }, networkError => console.log(networkError.message)
+      ).then(jsonResponse => {
+        console.log('made it!');
+      });
+      
+    });
+    */
 }
 
-let myRequest = new Request(loginGetRequestURL, myObj);
-
- export function loginUser(){
-   console.log('I\'m in loginUser!');
-    fetch(myRequest).then(response => {
-      if(response.ok){
-        console.log('get request is okay!');
-        return response.json();
-      }
-      throw new Error('Request failed!');
-      }, networkError => console.log(networkError.message)
-      ).then(jsonResponse => {
-        console.log('auth code: ' + jsonResponse);
-        fetch(loginPostRequestURL, {
-          method: 'POST',
-          body: JSON.stringify({id: '200'}),
-          grant_type: 'authorization_code',
-          code: jsonResponse.code,
-          redirect_uri: redirectUri,
-          client_id: clientId,
-          client_secret: clientSecret
-        }).then(response => {                  
-          if(response.ok) {
-            return response.json();                                                
-          }                                                                          
-          throw new Error('Request failed!');
-        }, networkError => console.log(networkError.message)
-        ).then(jsonResponse => {
-          console.log('made it!');
-        });
-        
-      });
-
-  }
-
   
-  export function getUserPlaylist(){
-    fetch(playlistURL).then(response => {
-      if(response.ok){
-        return response.json();
-      }
-      throw new Error('Request failed!');
-      }, networkError => console.log(networkError.message)
-      ).then(jsonResponse => {
-        //do something with repsonse
-    });
+export function getUserPlaylist(){
+  fetch(playlistURL).then(response => {
+    if(response.ok){
+      return response.json();
+    }
+    throw new Error('Request failed!');
+    }, networkError => console.log(networkError.message)
+    ).then(jsonResponse => {
+      //do something with repsonse
+  });
 
 
-  }
+}
 
 
 
